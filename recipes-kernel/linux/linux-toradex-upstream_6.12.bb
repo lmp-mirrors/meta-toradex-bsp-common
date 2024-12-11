lmp-mirrors/meta-toradex-bsp-common

commit bcff089f5b02cd868769fb9cfffc11127f0a433b

linux-toradex-upstream: upgrade from v6.11 to v6.12
Upgrade the Linux kernel from version v6.11 to v6.12 (LTS). Also, v6.12
includes the PREEMPT-RT patches already merged upstream for the ARM
architecure. This eliminates the need for patching the kernel with the
out-of-tree preempt-rt branch.

Related-to: ELB-6056
Signed-off-by: João Paulo Gonçalves <joao.goncalves@toradex.com>

recipes-kernel/linux/linux-toradex-upstream_6.12.bb
@@ -1,6 +1,6 @@
 require linux-toradex-upstream.inc
 
-FILESEXTRAPATHS:prepend := "${THISDIR}/linux-toradex-upstream-6.11:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/linux-toradex-upstream-6.12:"
 
 LINUX_REPO = "git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
 
@@ -14,12 +14,13 @@ TDX_PATCHES = " \
     file://0003-i2c-imx-prevent-rescheduling-in-non-dma-mode.patch \
     file://0004-arm64-dts-imx8mm-verdin-add-single-master-property-t.patch \
     file://0005-arm64-dts-imx8mp-verdin-add-single-master-property-t.patch \
-    file://0001-PCI-imx6-Add-suspend-resume-support-for-i.MX6QDL.patch \
 "
 
-PV = "6.11"
-LINUX_VERSION ?= "6.11.10"
-KBRANCH = "linux-6.11.y"
+PV = "6.12"
+LINUX_VERSION ?= "6.12.4"
+LINUX_KERNEL_TYPE:preempt-rt = "preempt-rt"
+LINUX_VERSION:preempt-rt = "6.12.4-rt"
+KBRANCH = "linux-6.12.y"
 KERNEL_VERSION_SANITY_SKIP = "1"
-SRCREV_machine = "e7770f0e399b8b6ccf46283e7887a3baf2cc2ca8"
+SRCREV_machine = "61baee2dc5341c936e7fa7b1ca33c5607868de69"
 SRCREV_machine:use-head-next = "${AUTOREV}"

recipes-kernel/linux/linux-toradex-upstream-6.11/0001-PCI-imx6-Add-suspend-resume-support-for-i.MX6QDL.patch
@@ -1,127 +1,0 @@
-From 394a8f246c1a44fd98869be7e8aa12803906f87b Mon Sep 17 00:00:00 2001
-From: Stefan Eichenberger <stefan.eichenberger@toradex.com>
-Date: Wed, 2 Oct 2024 16:26:06 +0200
-Subject: [PATCH] PCI: imx6: Add suspend/resume support for i.MX6QDL
-
-The suspend/resume functionality is currently broken on the i.MX6QDL
-platform, as documented in the NXP errata (ERR005723):
-https://www.nxp.com/docs/en/errata/IMX6DQCE.pdf
-
-This patch addresses the issue by sharing most of the suspend/resume
-sequences used by other i.MX devices, while avoiding modifications to
-critical registers that disrupt the PCIe functionality. It targets the
-same problem as the following downstream commit:
-https://github.com/nxp-imx/linux-imx/commit/4e92355e1f79d225ea842511fcfd42b343b32995
-
-Unlike the downstream commit, this patch also resets the connected PCIe
-device if possible. Without this reset, certain drivers, such as ath10k
-or iwlwifi, will crash on resume. The device reset is also done by the
-driver on other i.MX platforms, making this patch consistent with
-existing practices.
-
-Without this patch, suspend/resume will fail on i.MX6QDL devices if a
-PCIe device is connected. Upon resuming, the kernel will hang and
-display an error. Here's an example of the error encountered with the
-ath10k driver:
-ath10k_pci 0000:01:00.0: Unable to change power state from D3hot to D0, device inaccessible
-Unhandled fault: imprecise external abort (0x1406) at 0x0106f944
-
-Upstream-Status: Submitted [https://lore.kernel.org/all/20241030103250.83640-1-eichest@gmail.com/]
-Signed-off-by: Stefan Eichenberger <stefan.eichenberger@toradex.com>
----
- drivers/pci/controller/dwc/pci-imx6.c | 57 +++++++++++++++++++++------
- 1 file changed, 46 insertions(+), 11 deletions(-)
-
-diff --git a/drivers/pci/controller/dwc/pci-imx6.c b/drivers/pci/controller/dwc/pci-imx6.c
-index eaec471c4623..bfcba37c1791 100644
---- a/drivers/pci/controller/dwc/pci-imx6.c
-+++ b/drivers/pci/controller/dwc/pci-imx6.c
-@@ -79,6 +79,11 @@ enum imx6_pcie_variants {
- #define IMX6_PCIE_FLAG_HAS_PHY_RESET		BIT(5)
- #define IMX6_PCIE_FLAG_HAS_SERDES		BIT(6)
- #define IMX6_PCIE_FLAG_SUPPORT_64BIT		BIT(7)
-+/*
-+ * Because of ERR005723 (PCIe does not support L2 power down) we need to
-+ * workaround suspend resume on some devices which are affected by this errata.
-+ */
-+#define IMX6_PCIE_FLAG_BROKEN_SUSPEND		BIT(8)
- 
- #define imx6_check_flag(pci, val)     (pci->drvdata->flags & val)
- 
-@@ -1203,9 +1208,19 @@ static int imx6_pcie_suspend_noirq(struct device *dev)
- 		return 0;
- 
- 	imx6_pcie_msi_save_restore(imx6_pcie, true);
--	imx6_pcie_pm_turnoff(imx6_pcie);
--	imx6_pcie_stop_link(imx6_pcie->pci);
--	imx6_pcie_host_exit(pp);
-+	if (imx6_check_flag(imx6_pcie, IMX6_PCIE_FLAG_BROKEN_SUSPEND)) {
-+		/*
-+		 * The minimum for a workaround would be to set PERST# and to
-+		 * set the PCIE_TEST_PD flag. However, we can also disable the
-+		 * clock which saves some power.
-+		 */
-+		imx6_pcie_assert_core_reset(imx6_pcie);
-+		imx6_pcie_disable_ref_clk(imx6_pcie);
-+	} else {
-+		imx6_pcie_pm_turnoff(imx6_pcie);
-+		imx6_pcie_stop_link(imx6_pcie->pci);
-+		imx6_pcie_host_exit(pp);
-+	}
- 
- 	return 0;
- }
-@@ -1219,14 +1234,32 @@ static int imx6_pcie_resume_noirq(struct device *dev)
- 	if (!(imx6_pcie->drvdata->flags & IMX6_PCIE_FLAG_SUPPORTS_SUSPEND))
- 		return 0;
- 
--	ret = imx6_pcie_host_init(pp);
--	if (ret)
--		return ret;
--	imx6_pcie_msi_save_restore(imx6_pcie, false);
--	dw_pcie_setup_rc(pp);
-+	if (imx6_check_flag(imx6_pcie, IMX6_PCIE_FLAG_BROKEN_SUSPEND)) {
-+		ret = imx6_pcie_enable_ref_clk(imx6_pcie);
-+		if (ret)
-+			return ret;
-+		ret = imx6_pcie_deassert_core_reset(imx6_pcie);
-+		if (ret)
-+			return ret;
-+		/*
-+		 * Using PCIE_TEST_PD seems to disable MSI and powers down the
-+		 * root complex. This is why we have to setup the rc again and
-+		 * why we have to restore the MSI register.
-+		 */
-+		ret = dw_pcie_setup_rc(&imx6_pcie->pci->pp);
-+		if (ret)
-+			return ret;
-+		imx6_pcie_msi_save_restore(imx6_pcie, false);
-+	} else {
-+		ret = imx6_pcie_host_init(pp);
-+		if (ret)
-+			return ret;
-+		imx6_pcie_msi_save_restore(imx6_pcie, false);
-+		dw_pcie_setup_rc(pp);
- 
--	if (imx6_pcie->link_is_up)
--		imx6_pcie_start_link(imx6_pcie->pci);
-+		if (imx6_pcie->link_is_up)
-+			imx6_pcie_start_link(imx6_pcie->pci);
-+	}
- 
- 	return 0;
- }
-@@ -1450,7 +1483,9 @@ static const struct imx6_pcie_drvdata drvdata[] = {
- 	[IMX6Q] = {
- 		.variant = IMX6Q,
- 		.flags = IMX6_PCIE_FLAG_IMX6_PHY |
--			 IMX6_PCIE_FLAG_IMX6_SPEED_CHANGE,
-+			 IMX6_PCIE_FLAG_IMX6_SPEED_CHANGE |
-+			 IMX6_PCIE_FLAG_BROKEN_SUSPEND |
-+			 IMX6_PCIE_FLAG_SUPPORTS_SUSPEND,
- 		.dbi_length = 0x200,
- 		.gpr = "fsl,imx6q-iomuxc-gpr",
- 		.clk_names = imx6q_clks,
--- 
-2.43.0
-

commit 204b52fc559dbba86effadfe1cf2dc5f4bf24c71

linux-toradex-upstream: remove preempt-rt version override
There is no need to override the kernel version when using PREEMPT-RT.
As of kernel version 6.12, PREEMPT-RT is only a configuration
difference compared to the non-RT kernel. Users can rely on alternative
mechanisms to identify whether a real-time kernel is in use.

Related-to: ELB-6056
Fixes: bcff089f5b02 ("linux-toradex-upstream: upgrade from v6.11 to v6.12")
Signed-off-by: João Paulo Gonçalves <joao.goncalves@toradex.com>

recipes-kernel/linux/linux-toradex-upstream_6.12.bb
@@ -19,7 +19,6 @@ TDX_PATCHES = " \
 PV = "6.12"
 LINUX_VERSION ?= "6.12.4"
 LINUX_KERNEL_TYPE:preempt-rt = "preempt-rt"
-LINUX_VERSION:preempt-rt = "6.12.4-rt"
 KBRANCH = "linux-6.12.y"
 KERNEL_VERSION_SANITY_SKIP = "1"
 SRCREV_machine = "61baee2dc5341c936e7fa7b1ca33c5607868de69"