6dae62145d325e6bb54c3591158df15210ad2930
Ernest Van Hoecke <ernest.vanhoecke@toradex.com>
2025-02-13 11:27:52 +0100

    colibri-imx6: udev: properly symlink ADCs in SoM v1.2
    The Colibri iMX6 v1.2 switched from the EOL STMPE811 ADC to the TLA2024.
    
    The mapping of the standardized names (colibri-ainX) to the ADC raw data
    files is different. To handle this transparently, parse the driver name
    in the toradex-adc.sh script and change the mapping accordingly.
    
    Related-to: ELB-5892
    Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>
    (cherry picked from commit a09544c57ffb776d8a2bdfdb35e567df6b32cccb)

--- recipes-core/udev/files/colibri-imx6/toradex-adc.sh
+++ recipes-core/udev/files/colibri-imx6/toradex-adc.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env sh
 #
 # Called from udev. Compatible to colibri-imx6.
+# Starting from v1.2 the STMPE811 is replaced by the TLA2024.
 #
 # Create/remove symlinks to/from the files with raw ain data.
 
 # Map the ADC lines:
-# colibri-ain{0..3} -> STMPE811 ADC{4,5,6,7} in terms of the driver.
+#  < v1.2: colibri-ain{0..3} -> STMPE811 ADC{4,5,6,7} in terms of the driver.
+# >= v1.2: colibri-ain{0..3} ->  TLA2024 ADC{0,1,2,3} in terms of the driver.
+
 LINES=4
-START=4
+
+# 4 for STMPE811, 0 for TLA2024
+START=0
+if [ "$(cat /sys$DEVPATH/name)" = "stmpe-adc" ]; then
+    START=4
+fi
 
 if [ "$ACTION" = "add" ]; then
     for idx in `seq 0 $((LINES-1))`; do
