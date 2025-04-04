lmp-mirrors/meta-toradex-bsp-common

commit 24f5d022c4bed21e8cb7af23a9e37ec20b13e290

udev-toradex-rules: [aquila|smarc]: set fan PWM to 100%
Add udev rule to force 100% fan PWM on aquila and smarc development
board. There is not support to control the fan based on the SoC
temperature, and by default the fan will not spin at all unless we
manually force it.

Related-to: ELB-6200
Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

recipes-core/udev/udev-toradex-rules.bb
@@ -15,14 +15,17 @@ SRC_URI:append:aquila-am69 = " \
     file://10-toradex-can1-ifname.link \
     file://10-toradex-can2-ifname.link \
     file://10-toradex-can3-ifname.link \
+    file://99-toradex-fan-pwm.rules \
 "
 SRC_URI:append:toradex-smarc-imx8mp = " \
     file://10-toradex-can0-ifname.link \
     file://10-toradex-can1-ifname.link \
+    file://99-toradex-fan-pwm.rules \
 "
 SRC_URI:append:toradex-smarc-imx95 = " \
     file://10-toradex-can0-ifname.link \
     file://10-toradex-can1-ifname.link \
+    file://99-toradex-fan-pwm.rules \
 "
 SRC_URI:append:verdin-am62 = " \
     file://10-toradex-can0-ifname.link \

recipes-core/udev/files/aquila-am69/99-toradex-fan-pwm.rules
@@ -1,0 +1,1 @@
+ACTION=="add", SUBSYSTEM=="hwmon", DRIVERS=="amc6821", ATTR{pwm1_enable}="1", ATTR{pwm1}="255"

recipes-core/udev/files/toradex-smarc-imx8mp/99-toradex-fan-pwm.rules
@@ -1,0 +1,1 @@
+ACTION=="add", SUBSYSTEM=="hwmon", DRIVERS=="amc6821", ATTR{pwm1_enable}="1", ATTR{pwm1}="255"

recipes-core/udev/files/toradex-smarc-imx95/99-toradex-fan-pwm.rules
@@ -1,0 +1,1 @@
+ACTION=="add", SUBSYSTEM=="hwmon", DRIVERS=="amc6821", ATTR{pwm1_enable}="1", ATTR{pwm1}="255"

commit cedaeabc075645450b56691e5cb944f7016a0809

udev-toradex-rules: [aquila|smarc]: install fan rules
Install fan rules, when the file was added it was forgot to add it to
the package.

Related-to: ELB-6200
Fixes: 24f5d022c4be ("udev-toradex-rules: [aquila|smarc]: set fan PWM to 100%")
Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

recipes-core/udev/udev-toradex-rules.bb
@@ -60,6 +60,9 @@ do_install () {
     if [ -f ${WORKDIR}/10-toradex-can3-ifname.link ]; then
         install -m 0644 ${WORKDIR}/10-toradex-can3-ifname.link ${D}${sysconfdir}/systemd/network/
     fi
+    if [ -f ${WORKDIR}/99-toradex-fan-pwm.rules ]; then
+        install -m 0644 ${WORKDIR}/99-toradex-fan-pwm.rules ${D}${sysconfdir}/systemd/network/
+    fi
     install -m 0644 ${WORKDIR}/10-toradex-wifi-ifnames.link ${D}${sysconfdir}/systemd/network/
     install -m 0644 ${WORKDIR}/99-toradex.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0644 ${WORKDIR}/bootpart-automount.rules ${D}${sysconfdir}/udev/rules.d/