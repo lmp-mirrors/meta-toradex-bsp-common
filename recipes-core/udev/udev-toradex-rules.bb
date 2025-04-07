lmp-mirrors/meta-toradex-bsp-common

commit cedaeabc075645450b56691e5cb944f7016a0809

udev-toradex-rules: [aquila|smarc]: install fan rules
Install fan rules, when the file was added it was forgot to add it to
the package.

Related-to: ELB-6200
Fixes: 24f5d022c4be ("udev-toradex-rules: [aquila|smarc]: set fan PWM to 100%")
Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

recipes-core/udev/udev-toradex-rules.bb
@@ -60,6 +60,9 @@ do_install () {
     if [ -f ${WORKDIR}/10-toradex-can3-ifname.link ]; then
         install -m 0644 ${WORKDIR}/10-toradex-can3-ifname.link ${D}${sysconfdir}/systemd/network/
     fi
+    if [ -f ${WORKDIR}/99-toradex-fan-pwm.rules ]; then
+        install -m 0644 ${WORKDIR}/99-toradex-fan-pwm.rules ${D}${sysconfdir}/systemd/network/
+    fi
     install -m 0644 ${WORKDIR}/10-toradex-wifi-ifnames.link ${D}${sysconfdir}/systemd/network/
     install -m 0644 ${WORKDIR}/99-toradex.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0644 ${WORKDIR}/bootpart-automount.rules ${D}${sysconfdir}/udev/rules.d/

commit 71d920dbcf2a585c4a0bf4318270403cb1866691

udev-toradex-rules: [aquila|smarc]: fix fan rules install dir
Fix fan PWM rule file installation directory.

Related-to: ELB-6200
Fixes: cedaeabc0756 ("udev-toradex-rules: [aquila|smarc]: install fan rules")
Fixes: 24f5d022c4be ("udev-toradex-rules: [aquila|smarc]: set fan PWM to 100%")
Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

recipes-core/udev/udev-toradex-rules.bb
@@ -61,7 +61,7 @@ do_install () {
         install -m 0644 ${WORKDIR}/10-toradex-can3-ifname.link ${D}${sysconfdir}/systemd/network/
     fi
     if [ -f ${WORKDIR}/99-toradex-fan-pwm.rules ]; then
-        install -m 0644 ${WORKDIR}/99-toradex-fan-pwm.rules ${D}${sysconfdir}/systemd/network/
+        install -m 0644 ${WORKDIR}/99-toradex-fan-pwm.rules ${D}${sysconfdir}/udev/rules.d/
     fi
     install -m 0644 ${WORKDIR}/10-toradex-wifi-ifnames.link ${D}${sysconfdir}/systemd/network/
     install -m 0644 ${WORKDIR}/99-toradex.rules ${D}${sysconfdir}/udev/rules.d/