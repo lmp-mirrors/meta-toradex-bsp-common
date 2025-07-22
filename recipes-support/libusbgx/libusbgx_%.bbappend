lmp-mirrors/meta-toradex-bsp-common

commit a32c71d07b264c05bd06c5dc0d03069f23f7ed9d

libusbgx: stop using usbgx.service
We now call gadget-start directly from udev, there is no reason for
using the usbgx service.

Using the service is less reliable than having a udev rule as explained
in commit "udev-toradex-rules: call gadget-start from udev".

Related-to: ELB-6355
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-support/libusbgx/libusbgx_%.bbappend
@@ -1,0 +1,1 @@
+SYSTEMD_AUTO_ENABLE:${PN}-examples = "disable"

commit 68af22811b793fce2bc2c796636e7620e641b80e

libusbgx: move libusbgx_%.bbappend to meta-toradex-demos
This bbappend belongs more with the reference image, move it there.

Related-to: ELB-6355
Fixes: a32c71d ("libusbgx: stop using usbgx.service")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-support/libusbgx/libusbgx_%.bbappend
@@ -1,1 +1,0 @@
-SYSTEMD_AUTO_ENABLE:${PN}-examples = "disable"