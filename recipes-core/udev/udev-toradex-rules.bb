lmp-mirrors/meta-toradex-bsp-common

commit b1838fddd62b6adc43c8b240e7ef5333c441f9bc

udev-toradex-rules: call gadget-start from udev
The systemd target `usb-gadget.target` is triggered by udev when a UDC
first comes up. The usbgx service uses this target and executes
gadget-start to initialize the gadget. It can happen that by the time
gadget-start runs, the UDC has been removed from the system again.

For example, we saw such a situation using the DWC3 USB controller and
usb-conn-gpio kernel modules as loadables.

By the time of the DWC3 init, udev was active, and during init DWC3
started the USB OTG port in devicemode, generating a udev add event.

If a pen drive was plugged in at boot, it would quickly switch to
host mode right after initialisation, emitting another udev event for
the removal of the UDC. The systemd target is thus reached, but by the
time gadget-start ran, the UDC was gone.

  dwc3 init       usb-conn-gpio role switch
      │                       │
      ▼                       ▼
udev: add UDC─┐         udev: del UDC─────►/sys/class/udc empty
              │                                         │
              │                                         x
              │                                         │
              │                                         ▼
              └────────────►usb-gadget.target─────►gadget-start

Call gadget-start from udev every time a UDC comes up instead of only
once via the systemd service (usbgx.service). Repeated runs of this
script are not problematic.

Related-to: ELB-6355
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-core/udev/files/99-toradex-usb-udc.rules
@@ -1,0 +1,1 @@
+ACTION=="add", SUBSYSTEM=="udc", RUN+="/usr/bin/gadget-start"

recipes-core/udev/udev-toradex-rules.bb
@@ -5,6 +5,7 @@ LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda
 SRC_URI = "\
     file://10-toradex-wifi-ifnames.link \
     file://99-toradex.rules \
+    file://99-toradex-usb-udc.rules \
     file://bootpart-automount.rules \
     file://toradex-adc.sh \
     file://toradex-mount-bootpart.sh \
@@ -63,6 +64,7 @@ do_install () {
     fi
     install -m 0644 ${WORKDIR}/10-toradex-wifi-ifnames.link ${D}${sysconfdir}/systemd/network/
     install -m 0644 ${WORKDIR}/99-toradex.rules ${D}${sysconfdir}/udev/rules.d/
+    install -m 0644 ${WORKDIR}/99-toradex-usb-udc.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0644 ${WORKDIR}/bootpart-automount.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0755 ${WORKDIR}/toradex-adc.sh ${D}${sysconfdir}/udev/scripts/
     install -m 0755 ${WORKDIR}/toradex-mount-bootpart.sh ${D}${sysconfdir}/udev/scripts/

commit cd50d5d96eb6505653c035c640a5c147380d6e1c

udev-toradex-rules: move USB UDC gadget rule to meta-toradex-demos
The udev rule calling gadget-start when a UDC appears only makes sense
if gadget-start is actually installed. This logic is also more relevant
to our reference images than our core BSP.

Therefore, install this rule as part of the libusbgx recipe instead of
as a default here. This libusbgx bbappend can be found in
meta-toradex-demos.

Related-to: ELB-6355
Fixes: b1838fd ("udev-toradex-rules: call gadget-start from udev")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-core/udev/files/99-toradex-usb-udc.rules
@@ -1,1 +1,0 @@
-ACTION=="add", SUBSYSTEM=="udc", RUN+="/usr/bin/gadget-start"

recipes-core/udev/udev-toradex-rules.bb
@@ -5,7 +5,6 @@ LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda
 SRC_URI = "\
     file://10-toradex-wifi-ifnames.link \
     file://99-toradex.rules \
-    file://99-toradex-usb-udc.rules \
     file://bootpart-automount.rules \
     file://toradex-adc.sh \
     file://toradex-mount-bootpart.sh \
@@ -64,7 +63,6 @@ do_install () {
     fi
     install -m 0644 ${WORKDIR}/10-toradex-wifi-ifnames.link ${D}${sysconfdir}/systemd/network/
     install -m 0644 ${WORKDIR}/99-toradex.rules ${D}${sysconfdir}/udev/rules.d/
-    install -m 0644 ${WORKDIR}/99-toradex-usb-udc.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0644 ${WORKDIR}/bootpart-automount.rules ${D}${sysconfdir}/udev/rules.d/
     install -m 0755 ${WORKDIR}/toradex-adc.sh ${D}${sysconfdir}/udev/scripts/
     install -m 0755 ${WORKDIR}/toradex-mount-bootpart.sh ${D}${sysconfdir}/udev/scripts/