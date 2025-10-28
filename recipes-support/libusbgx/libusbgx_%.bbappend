lmp-mirrors/meta-toradex-bsp-common

commit a46693192dd7c4c27d46a0d93807bf2d856d3ed0

Bump SRCREV
Bump SRCREV hashes as used in build of Fri 01 Aug 2025 09:50:30 PM UTC

Matrix Build: image-tdxref-scarthgap-7.x.y-nightly-matrix 402

Matrix Build Status: SUCCESS

Triggered by: https://jenkins.int.toradex.com/job/image-tdxref-scarthgap-7.x.y-nightly-matrix/402

Signed-off-by: ci.toradex <cicd@toradex.com>
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

recipes-support/libusbgx/libusbgx_%.bbappend
@@ -1,0 +1,1 @@
+SYSTEMD_AUTO_ENABLE:${PN}-examples = "disable"

recipes-core/udev/files/99-toradex-usb-udc.rules
@@ -1,0 +1,1 @@
+ACTION=="add", SUBSYSTEM=="udc", RUN+="/usr/bin/gadget-start"

recipes-kernel/linux/device-tree-overlays-mainline_git.bb
@@ -1,7 +1,7 @@
 inherit toradex-devicetree
 
 SRCBRANCH = "master"
-SRCREV = "3946ce4f19c955077ab9edc96004808177901a22"
+SRCREV = "034fe1e0943a044681b31cb07e9a7053cf76f6de"
 SRCREV:use-head-next = "${AUTOREV}"
 
 COMPATIBLE_MACHINE:tdx = ".*"

recipes-kernel/linux/linux-toradex-upstream-rt_6.6.bb
@@ -1,4 +1,4 @@
-LINUX_VERSION ?= "6.6.94-rt56"
+LINUX_VERSION ?= "6.6.99-rt58"
 require recipes-kernel/linux/linux-toradex-upstream_6.6.bb
 
 SUMMARY = "Toradex mainline real-time Linux kernel"
@@ -10,8 +10,8 @@ LINUX_REPO = "git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.g
 # set PV manually, that way PREFERRED_VERSION can be set to a constant value
 PV = "6.6"
 KBRANCH = "v6.6-rt"
-SRCREV_meta-toradex-bsp = "f4e5ca7902bbc59dd7b33f9c29d6a07b2f4b60db"
-SRCREV_machine = "d2cfda749eb5e01dd572b9e293dca7cfdb4ccb8a"
+SRCREV_meta-toradex-bsp = "57b5edcdc64c6625b045b663d8f3ff92b2dc627e"
+SRCREV_machine = "9ce90b76d5069580f8a3657c539be4c1b6ef9b4c"
 SRCREV_machine:use-head-next = "${AUTOREV}"
 
 LINUX_KERNEL_TYPE = "preempt-rt"

recipes-kernel/linux/linux-toradex-upstream.inc
@@ -28,7 +28,7 @@ SRC_URI = " \
 KCONFIG_REPO = "git://git.toradex.com/linux-toradex-kconfig.git"
 KMETA = "kernel-meta-toradex-bsp"
 
-SRCREV_meta-toradex-bsp = "f4e5ca7902bbc59dd7b33f9c29d6a07b2f4b60db"
+SRCREV_meta-toradex-bsp = "57b5edcdc64c6625b045b663d8f3ff92b2dc627e"
 SRCREV_meta-toradex-bsp:use-head-next = "${AUTOREV}"
 
 KERNEL_VERSION_SANITY_SKIP = "1"

recipes-kernel/linux/linux-toradex-upstream_6.12.bb
@@ -38,9 +38,9 @@ TDX_PATCHES = " \
 "
 
 PV = "6.12"
-LINUX_VERSION ?= "6.12.35"
+LINUX_VERSION ?= "6.12.41"
 LINUX_KERNEL_TYPE:preempt-rt = "preempt-rt"
 KBRANCH = "linux-6.12.y"
 KERNEL_VERSION_SANITY_SKIP = "1"
-SRCREV_machine = "783cd2c3dca8b6c434e955b84c20c8940588dc68"
+SRCREV_machine = "8f5ff9784f3262e6e85c68d86f8b7931827f2983"
 SRCREV_machine:use-head-next = "${AUTOREV}"

recipes-kernel/linux/linux-toradex-upstream_6.6.bb
@@ -64,8 +64,8 @@ TDX_PATCHES = " \
     file://0001-wifi-mwifiex-discard-erroneous-disassoc-frames-on-ST.patch \
 "
 PV = "6.6"
-LINUX_VERSION ?= "6.6.95"
+LINUX_VERSION ?= "6.6.101"
 KBRANCH = "linux-6.6.y"
 KERNEL_VERSION_SANITY_SKIP = "1"
-SRCREV_machine = "3f5b4c104b7d3267d015daf9d9681c5fe3b01224"
+SRCREV_machine = "3a8ababb8b6a0ced2be230b60b6e3ddbd8d67014"
 SRCREV_machine:use-head-next = "${AUTOREV}"

recipes-kernel/linux/linux-toradex-upstream_mainline.bb
@@ -14,6 +14,6 @@ TDX_PATCHES = " \
 "
 # set PV manually, that way PREFERRED_VERSION can be set to a constant value
 PV = "mainline"
-LINUX_VERSION = "6.14-rc"
+LINUX_VERSION = "6.16"
 KBRANCH = "master"
 SRCREV_machine = "${AUTOREV}"

commit 940c40002146016f5f40dc0c9e4702a3ef30be6b

libusbgx: remove libusbgx_%.bbappend
This file is already present in meta-toradex-demos, where it should be.
It was moved from this layer to there, but this file was added back in
by a bump of the sources.

Fix that by removing it again.

Related-to: ELB-6355
Fixes: a466931 ("Bump SRCREV")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-support/libusbgx/libusbgx_%.bbappend
@@ -1,1 +1,0 @@
-SYSTEMD_AUTO_ENABLE:${PN}-examples = "disable"