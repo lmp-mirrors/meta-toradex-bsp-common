lmp-mirrors/meta-toradex-bsp-common

commit c50a9bbef165b1235b3582effe4e23866d37071f

linux-firmware: cleanup recipe
Prepare for future changes by cleaning up this recipe. The iMX95 and
AM69 specific do_install appends share code that can be separated for
clarity and maintainability, do so.

PACKAGE_ARCH cannot be set as the base recipe linux-firmware inherits
the allarch class. When building for multiple machines where some of
them are the Aquila AM69 or Aquila iMX95, this will lead to rebuilding
the linux-firmware package when switching machine, because the
PACKAGE_ARCH is set to all.

Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>
(cherry picked from commit e214ec19a258edc5105dcd0f9f7b36ff2c402403)

recipes-kernel/linux-firmware/linux-firmware_%.bbappend
@@ -1,22 +1,29 @@
-SRC_URI:append:aquila-am69 = " https://docs.toradex.com/116005-sx-pceax-board-files_v1.0.zip;name=sx-pceax-board-files \
-                               https://docs.toradex.com/117028-sx-pcebe-board-files_v1.0.zip;name=sx-pcebe-board-files"
-SRC_URI:append:aquila-imx95 = " https://docs.toradex.com/117028-sx-pcebe-board-files_v1.0.zip;name=sx-pcebe-board-files"
+SRC_URI:append = " https://docs.toradex.com/116005-sx-pceax-board-files_v1.0.zip;name=sx-pceax-board-files \
+                   https://docs.toradex.com/117028-sx-pcebe-board-files_v1.0.zip;name=sx-pcebe-board-files"
 SRC_URI[sx-pceax-board-files.sha256sum] = "f94f6382ac1673b84a17e424b5bff681a108eea869544d9e3a3d804f498ae28f"
 SRC_URI[sx-pcebe-board-files.sha256sum] = "a74d2acc8bab70c8cdd070f3dbbc3f5571b0eb1591f029ff4986ea47387d8d71"
 
 ATH11K_FIRMWARE_PATH = "${nonarch_base_libdir}/firmware/ath11k/WCN6855/hw2.1"
 ATH12K_FIRMWARE_PATH = "${nonarch_base_libdir}/firmware/ath12k/WCN7850/hw2.0"
 
-do_install:append:aquila-am69 () {
-	install -m 0644 ${UNPACKDIR}/sx-pceax-board-files/board-2_US_EU_JP.bin ${D}${ATH11K_FIRMWARE_PATH}
-	install -m 0644 ${UNPACKDIR}/sx-pceax-board-files/board-2_UK_CA.bin    ${D}${ATH11K_FIRMWARE_PATH}
-	install -m 0644 ${UNPACKDIR}/sx-pcebe-board-files/board-2.bin          ${D}${ATH12K_FIRMWARE_PATH}
+install_sx_pceax_board_files () {
+        install -m 0644 ${WORKDIR}/sx-pceax-board-files/board-2_US_EU_JP.bin ${D}${ATH11K_FIRMWARE_PATH}
+        install -m 0644 ${WORKDIR}/sx-pceax-board-files/board-2_UK_CA.bin    ${D}${ATH11K_FIRMWARE_PATH}
 
-	rm -f ${D}${ATH11K_FIRMWARE_PATH}/board-2.bin
-	ln -frs ${D}${ATH11K_FIRMWARE_PATH}/board-2_US_EU_JP.bin ${D}${ATH11K_FIRMWARE_PATH}/board-2.bin
+        rm -f ${D}${ATH11K_FIRMWARE_PATH}/board-2.bin
+        ln -frs ${D}${ATH11K_FIRMWARE_PATH}/board-2_US_EU_JP.bin ${D}${ATH11K_FIRMWARE_PATH}/board-2.bin
+}
+
+install_sx_pcebe_board_files () {
+        install -m 0644 ${WORKDIR}/sx-pcebe-board-files/board-2.bin          ${D}${ATH12K_FIRMWARE_PATH}
+}
+
+do_install:append:aquila-am69 () {
+        install_sx_pceax_board_files
+        install_sx_pcebe_board_files
 }
 
 do_install:append:aquila-imx95 () {
-	install -m 0644 ${UNPACKDIR}/sx-pcebe-board-files/board-2.bin          ${D}${ATH12K_FIRMWARE_PATH}
+        install_sx_pcebe_board_files
 }
 

commit 191ff0975bd6ec4fb5f973b605154bf7400d45d6

linux-firmware: change WORKDIR to UNPACKDIR
During the forward port of this recipe's cleanup and updating the
SX-PCEBE board files, UNPACKDIR was mistakenly changed back to WORKDIR,
fix that.

Fixes: c50a9bb ("linux-firmware: cleanup recipe")
Fixes: 5051b9d ("linux-firmware: update ath12k firmware binaries")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-kernel/linux-firmware/linux-firmware_%.bbappend
@@ -7,17 +7,17 @@ ATH11K_FIRMWARE_PATH = "${nonarch_base_libdir}/firmware/ath11k/WCN6855/hw2.1"
 ATH12K_FIRMWARE_PATH = "${nonarch_base_libdir}/firmware/ath12k/WCN7850/hw2.0"
 
 install_sx_pceax_board_files () {
-        install -m 0644 ${WORKDIR}/sx-pceax-board-files/board-2_US_EU_JP.bin ${D}${ATH11K_FIRMWARE_PATH}
-        install -m 0644 ${WORKDIR}/sx-pceax-board-files/board-2_UK_CA.bin    ${D}${ATH11K_FIRMWARE_PATH}
+        install -m 0644 ${UNPACKDIR}/sx-pceax-board-files/board-2_US_EU_JP.bin ${D}${ATH11K_FIRMWARE_PATH}
+        install -m 0644 ${UNPACKDIR}/sx-pceax-board-files/board-2_UK_CA.bin    ${D}${ATH11K_FIRMWARE_PATH}
 
         rm -f ${D}${ATH11K_FIRMWARE_PATH}/board-2.bin
         ln -frs ${D}${ATH11K_FIRMWARE_PATH}/board-2_US_EU_JP.bin ${D}${ATH11K_FIRMWARE_PATH}/board-2.bin
 }
 
 install_sx_pcebe_board_files () {
-        install -m 0644 ${WORKDIR}/sx-pcebe-board-files/board-2_US_EU_JP.bin   ${D}${ATH12K_FIRMWARE_PATH}
-        install -m 0644 ${WORKDIR}/sx-pcebe-board-files/board-2_UK_CA_high.bin ${D}${ATH12K_FIRMWARE_PATH}
-        install -m 0644 ${WORKDIR}/sx-pcebe-board-files/board-2_UK_CA_low.bin  ${D}${ATH12K_FIRMWARE_PATH}
+        install -m 0644 ${UNPACKDIR}/sx-pcebe-board-files/board-2_US_EU_JP.bin   ${D}${ATH12K_FIRMWARE_PATH}
+        install -m 0644 ${UNPACKDIR}/sx-pcebe-board-files/board-2_UK_CA_high.bin ${D}${ATH12K_FIRMWARE_PATH}
+        install -m 0644 ${UNPACKDIR}/sx-pcebe-board-files/board-2_UK_CA_low.bin  ${D}${ATH12K_FIRMWARE_PATH}
 
         rm -f ${D}${ATH12K_FIRMWARE_PATH}/board-2.bin
         ln -frs ${D}${ATH12K_FIRMWARE_PATH}/board-2_US_EU_JP.bin ${D}${ATH12K_FIRMWARE_PATH}/board-2.bin